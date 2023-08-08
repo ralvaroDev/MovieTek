53eec3a5754ed1a1898294a853e046de6c2a993e
Alvaro Ramirez <ralvaro.dev@gmail.com>
2023-08-07 21:40:04 -0500

    feat: implement Navigation Components to simplify app navigation
    Main navigation is added, it will contain the main navigation screens like search, detail, saved movies...

---
+++ app/src/main/java/pe/ralvaro/movietek/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package pe.ralvaro.movietek.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import pe.ralvaro.movietek.R
+import pe.ralvaro.movietek.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+
+}
---
+++ app/src/main/java/pe/ralvaro/movietek/ui/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package pe.ralvaro.movietek.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pe.ralvaro.movietek.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
